22bcab24ec5caf088e9c666eec2197ac3028fa9b
BellaBe <bella.belgarokova@hotmail.com>
2018-09-06 11:00:09 +0200

    commit projects page

--- server.js
+++ server.js
@@ -42,6 +42,12 @@ app.get('/about', (req, res)=>{
         pageTitle: 'About page',
     })
 });
+
+app.get('/projects', (req, res)=>{
+    res.render('projects', {
+        pageTitle: 'Projects page'
+    })
+})
 app.get('/bad', (req, res)=>{
     res.send({
         errorMessage: 'Unable to handle request'
